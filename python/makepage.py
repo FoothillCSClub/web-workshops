1bfa778627382edbbe9ce4914bc16dcecc4d1002
fractalbach <4bach@protonmail.com>
2018-04-26 06:15:43 -0700

    python script for web templates

--- python/makepage.py
+++ python/makepage.py
@@ -1,1 +1,47 @@
-# template.py
+# makepage.py
+
+
+import argparse
+
+
+default_title = 'my awesome page'
+default_body = '''
+Here is a bunch of information that will go into the webpage.
+'''
+
+
+parser = argparse.ArgumentParser(description='Process a template.')
+
+parser.add_argument('-o',
+                    dest='output',
+                    required='true',
+                    help='output location to write html file.')
+
+parser.add_argument('input', 
+                    help='filename and path of the template to process.')
+
+parser.add_argument('--title', '-t',
+                    default=default_title,
+                    help='Title of the webpage.')
+
+parser.add_argument('--body', '-b',
+                    default=default_body ,
+                    help='Body of the webpage.')
+
+args = parser.parse_args()
+title = args.title
+body = args.body
+
+
+# Information that gets inserted into the template.
+
+
+
+with open(args.input) as f:
+    data = f.read()
+
+html_output = data.format(title=title, body=body)
+
+
+with open(args.output, 'w+') as f:
+    f.write(html_output)
